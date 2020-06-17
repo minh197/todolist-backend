68ec80630e59bf9ad16855193e968934b54ecb97
Minh Nguyen <gin411@Minhs-MacBook-Pro-2.local>
2020-06-16 23:28:52 -0400

    Third commit

--- app.js
+++ app.js
@@ -30,8 +30,40 @@ function addToDo(todo, status) {
     saveData(data)
 
 }
-
-
+yargs.command ({
+    command: "delete",
+    describe: "deletes one todo item",
+    builder: {
+        id: {
+            describe: "id number",
+            demandOption: true,
+            type: "number",
+        }
+    },
+    handler: function(builder) {
+        console.log(chalk.red.bold("One todo has been deleted."))
+        let data = loadData()
+        data.splice(builder.id,1)
+        saveData(data)
+    }
+})
+yargs.command ({
+    command: "toggle",
+    describe: "toggles todos from complete to incomplete and vice versa",
+    builder: {
+        id: {
+            describe: "id number",
+            demandOption: true,
+            type: "number",
+        }
+    },
+    handler: function(builder) {
+        console.log(chalk.green.bold("Your todo has been toggled."))
+        let data = loadData()
+        data[builder.id].status = !data[builder.id].status
+        saveData(data)
+    }
+})
 
 yargs.command({
     command: "list",
@@ -56,30 +88,34 @@ yargs.command({
             console.log(chalk.yellow.bold("Listing all the to-dos"))
     }
 });
-yargs.command({
-    command: "complete",
-    describe: "show a completed todos",
-    builder:{
-        status:{
-        describe: "Describe the status of the to-do",
-        demandOption: false,
-        type: "boolean",
-        default: "false"
-    }
-    },
-    
-    handler: function(args){
-        const todos = loadData()
-       for(let {todo, status} of todos) {
-           if(status=args.status){
-               console.log(chalk.bold.blue(todo,status))
 
-           }
-       }
-       
+yargs.command ({
+    command: "list_complete",
+    describe: "listing completed todos",
+    handler: function() {
+        console.log(chalk.green.bold("Here are all the completed todos."))
+        const data = loadData();
+        data.forEach(({id, todo, status}) => {if (status === true) {console.log(`
+        id: ${id}
+        todo: ${todo}
+        status: ${status}`)}})
     }
+
 })
 
+yargs.command ({
+    command: "list_incomplete",
+    describe: "listing completed todos",
+    handler: function() {
+        console.log(chalk.magenta.bold("Here are all the incompleted todos."))
+        const data = loadData();
+        data.forEach(({id, todo, status}) => {if (status === false) {console.log(`
+        id: ${id}
+        todo: ${todo}
+        status: ${status}`)}})
+    }
+
+})
 
 yargs.command({
     command: "add",
